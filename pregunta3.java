7834662685d55f36f024d61f5e6f07050d580476
LuisMi01 <113381771+LuisMi01@users.noreply.github.com>
2023-02-02 18:48:23 +0000

    Segunda version de mii codigo

--- pregunta3.java
+++ pregunta3.java
@@ -1,1 +1,11 @@
 //* ¿Haz hecho alguna vez un algoritmo para un programa? ¿Qué hacía? ¿Era correcto y eficiente?
+
+
+//codigo de java para el algoritmo de ordenamiento de burbuja de la forma recursiva con orientada a objetos
+
+class pregunta3 {
+    public static void main(String[] args) {
+        System.out.println("¿Haz hecho alguna vez un algoritmo para un programa? ¿Qué hacía? ¿Era correcto y eficiente?");
+    }
+}
+
